6ab1643309c74fbcd1e5f3639fa0c5fc29acb7ad
matushchenko <matushchenko@yandex.ru>
2021-05-23 21:24:27 +0300

    Домашняя работа 1

---
+++ HomeWork1/Tasks/Task3/Program.cs
@@ -0,0 +1,62 @@
+﻿/* Матущенко Наталья
+3.
+а) Написать программу, которая подсчитывает расстояние между точками 
+с координатами x1, y1 и x2,y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2).
+Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+б) *Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+*/
+using System;
+
+namespace Task3
+{
+    class Program
+
+    {
+        static void Task_3a()
+        {
+            Console.WriteLine("Укажите координату x1:");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите координату y1:");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите координату x2:");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите координату y2:");
+            int y2 = int.Parse(Console.ReadLine());
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Расстояние между точками: {r:F2}");
+            Console.ReadKey();
+        }
+
+        static void Task_math(int x1, int x2, int y1, int y2)
+        {
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Расстояние между точками: {r:F2}");
+        }
+
+        static void Task_3b()
+        {
+            Console.WriteLine("Укажите координату x1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите координату y1:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите координату x2:");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите координату y2:");
+            int d = int.Parse(Console.ReadLine());
+            Task_math(a, b, c, d);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матущенко Наталья. Задача 3-а: Написать программу, которая подсчитывает расстояние между точками." +
+                "Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой).");
+            Task_3a();
+            Console.WriteLine(" ");
+            Console.WriteLine("Матущенко Наталья. Задача 3-b : Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.");
+            Task_3b();
+            Console.WriteLine(" ");
+            Console.WriteLine("Конец 3 задачи.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork1/Tasks/Task4/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Матущенко Наталья
+4. Написать программу обмена значениями двух переменных типа int без использования вспомогательных методов.
+а) с использованием третьей переменной;
+б) *без использования третьей переменной.
+*/
+
+using System;
+
+namespace Task4
+{
+    class Program
+    {
+        // с использованием третьей переменной;
+        static void Task_4a()
+        {
+            
+            Console.WriteLine("Введите значение a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Первоначальные значения: " + " a = " + a + " b = " + b);
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Итоговые значения: " + " a = " + a + " b = " + b);
+        }
+
+
+        //б) *без использования третьей переменной;
+        static void Task_4b()
+         {
+            
+            Console.WriteLine("Введите значение a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Первоначальные значения: " + " a = " + a + " b = " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("Итоговые значения: " + " a = " + a + " b = " + b);
+         }
+
+        static void Main()
+        {
+            Console.WriteLine("Матущенко Наталья. Задача 4-а: обмена значениями двух переменных с использованием третьей переменной");
+            Task_4a();
+            Console.WriteLine(" ");
+            Console.WriteLine("Матущенко Наталья. Задача 4-b : обмена значениями двух переменных без использования третьей переменной");
+            Task_4b();
+            Console.WriteLine(" ");
+            Console.WriteLine("Конец 4 задачи.");
+            Console.ReadKey();
+
+
+        }
+                
+        }
+
+
+
+    }
+
