24052ddac59d8d93ce4ea392d0b6dcaa8479007d
matushchenko <matushchenko@yandex.ru>
2021-05-28 14:51:57 +0300

    Первые 5 заданий ДЗ 2

---
+++ HomeWork2/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int a, count = 0;
+			do
+			{
+				Console.Write("Введите возраст:");
+				a = int.Parse(Console.ReadLine());
+				count++;
+			}
+			while (a < 1 || a > 99);// Повторять пока условие истинно(true)
+			Console.WriteLine("Вы сделали " + count + " попыток ввода");
+		}
+
+	}
+}
